5d3eb60d01ca8e6bbe55ea48b905835acb7f4e7b
joacopichetto-dev <joacopiche12@gmail.com>
2026-04-03 14:32:17 -0300

    cambios en el ejercicio10

--- src/ejercicio10.py
+++ src/ejercicio10.py
@@ -35,16 +35,25 @@ def competencia_cocina(rounds):
                 ganador=nombre
         
         #sumamos victoria
-        stats[nombre]["wins"]+=1
+        stats[ganador]["wins"]+=1
         print(f"Ganador: {ganador} ({mejor_puntaje} pts)")
          # tabla parcial
         print("Posiciones parciales:")
         ordenados = sorted(stats.items(), key=lambda x: x[1]["total"], reverse=True)
+        
+        for nombre, datos in ordenados:
+            promedio = datos["total"] / datos["rounds"]
+            print(f"{nombre} | {datos['total']} | {datos['wins']} | {datos['best']} | {round(promedio,1)}")
     #tabla final
 
     print("\nTabla de posiciones final:")
     print("Cocinero | Puntaje | Rondas ganadas | Mejor ronda | Promedio")
     #ordenamos por puntaje total, de mayor a menor
+    ordenados = sorted(stats.items(), key=lambda x: x[1]["total"], reverse=True)
+
+    for nombre, datos in ordenados:
+        promedio = datos["total"] / datos["rounds"]
+        print(f"{nombre} | {datos['total']} | {datos['wins']} | {datos['best']} | {round(promedio,1)}")
     ordenados = sorted(stats.items(), key=lambda x: x[1]["total"], reverse=True)
 
     for nombre, datos in ordenados:
